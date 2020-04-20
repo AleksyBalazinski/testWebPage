d325419a65683d3d09db92698c4665523753009b
AleksyBalazinski <54417806+AleksyBalazinski@users.noreply.github.com>
2020-04-20 20:25:42 +0200

    Add files via upload

---
+++ test/test.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>MathJax example</title>
+  <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+  <script id="MathJax-script" async
+          src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js">
+  </script>
+</head>
+<body>
+<h1>To jest strona testowa</h1>
+<h2>Wyświetlamy tutaj równania matematyczne napisane z użyciem MathJax</h2>
+<p>
+  When \(a \ne 0\), there are two solutions to \(ax^2 + bx + c = 0\) and they are
+  \[x = {-b \pm \sqrt{b^2-4ac} \over 2a}.\]
+  When $$\sin{xd}=3$$
+</p>
+</body>
+</html>
